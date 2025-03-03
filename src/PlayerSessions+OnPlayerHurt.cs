4748e63ee555446598afd62b0c8dabe4333f304d
Kalle <info@kalleminkner.de>
2025-03-03 12:30:28 +0100

    fix: OnPlayerHurt

--- src/PlayerSessions+OnPlayerHurt.cs
+++ src/PlayerSessions+OnPlayerHurt.cs
@@ -12,44 +12,31 @@ namespace Challenges
                 || !attacker.IsValid
                 || victim == null
                 || !victim.IsValid) return HookResult.Continue;
+            // create challenge data
+            Dictionary<string, string> challengeData = new Dictionary<string, string>
+            {
+                { "isduringround", _isDuringRound.ToString() },
+                { "isteamdamage", (attacker.TeamNum == victim.TeamNum).ToString() },
+                { "isselfdamage", (attacker == victim).ToString() },
+                { "attacker", attacker.PlayerName },
+                { "attacker_isbot", attacker.IsBot.ToString() },
+                { "attacker_team", attacker.Team.ToString() },
+                { "victim", victim.PlayerName },
+                { "victim_isbot", victim.IsBot.ToString() },
+                { "victim_team", victim.Team.ToString() },
+                { "dmghealth", @event.DmgHealth.ToString() },
+                { "dmgarmor", @event.DmgArmor.ToString() },
+                { "health", @event.Health.ToString() },
+                { "armor", @event.Armor.ToString() },
+                { "hitgroup", @event.Hitgroup.ToString() },
+                { "weapon", @event.Weapon },
+            };
             // check attacker for challenge
             if (_playerConfigs.ContainsKey(attacker.NetworkIDString))
-                CheckChallengeGoal(attacker, "player_hurt", new Dictionary<string, string>
-                {
-                    { "isduringround", _isDuringRound.ToString() },
-                    { "isteamdamage", (attacker.TeamNum == victim.TeamNum).ToString() },
-                    { "isselfdamage", (attacker == victim).ToString() },
-                    { "isattacker", "true" },
-                    { "attacker", attacker.PlayerName },
-                    { "attacker_isbot", attacker.IsBot.ToString() },
-                    { "attacker_team", attacker.Team.ToString() },
-                    { "isvictim", "false" },
-                    { "victim", victim.PlayerName },
-                    { "victim_isbot", victim.IsBot.ToString() },
-                    { "victim_team", victim.Team.ToString() },
-                    { "dmghealth", @event.DmgHealth.ToString() },
-                    { "dmgarmor", @event.DmgArmor.ToString() },
-                    { "health", @event.Health.ToString() },
-                    { "armor", @event.Armor.ToString() },
-                    { "hitgroup", @event.Hitgroup.ToString() },
-                    { "weapon", @event.Weapon },
-                });
+                CheckChallengeGoal(attacker, "player_hurt_attacker", challengeData);
             // check victim for challenge
             if (_playerConfigs.ContainsKey(victim.NetworkIDString))
-                CheckChallengeGoal(victim, "player_hurt", new Dictionary<string, string>
-                {
-                    { "isduringround", _isDuringRound.ToString() },
-                    { "isteamdamage", (attacker.TeamNum == victim.TeamNum).ToString() },
-                    { "isselfdamage", (attacker == victim).ToString() },
-                    { "isattacker", "false" },
-                    { "attacker", attacker.PlayerName },
-                    { "attacker_isbot", attacker.IsBot.ToString() },
-                    { "attacker_team", attacker.Team.ToString() },
-                    { "isvictim", "true" },
-                    { "victim", victim.PlayerName },
-                    { "victim_isbot", victim.IsBot.ToString() },
-                    { "victim_team", victim.Team.ToString() },
-                });
+                CheckChallengeGoal(victim, "player_hurt_victim", challengeData);
             return HookResult.Continue;
         }
     }
