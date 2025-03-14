7cf8d7c7f91be19cbadda94b2c1d66e073a11124
Kalle <kalle@kandru.de>
2025-03-14 21:37:10 +0100

    bump version to 0.1.6

--- src/Challenges/Version.cs
+++ src/Challenges/Version.cs
@@ -4,6 +4,6 @@ namespace Challenges
 {
     public partial class Challenges : BasePlugin
     {
-        public override string ModuleVersion => "0.1.5";
+        public override string ModuleVersion => "0.1.6";
     }
 }
